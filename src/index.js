2f0152293814844c00a123bf847dc0adb3dd88a1
Yeobie <konoha.shoujo@gmail.com>
2024-03-01 18:20:13 +0100

    Added UX to the generator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function createCharacter(response) {
-  new Typewriter("#character", {
+  new Typewriter("#character-description", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -14,12 +14,15 @@ function generateCharacter(event) {
   let key = "f05bbfeo3a24e0d00t5941bd06aa34a4";
   let prompt = `Generate a new character that is defined by the word: ${keywords.value}`;
   let context =
-    "You are an AI with lots of imagination and you love to imagine amazing stories with amazing characters. Write a description, in basic HTML, of a character of a new story, it can be the main character or a sidekick or a villain. It can be any genre of story. Make it short and concise, no more than 600 characters long. Don't put any images.";
+    "You are an AI with lots of imagination and you love to imagine amazing stories with amazing characters. Write a description, in basic HTML, of a character of a new story, it can be the main character or a sidekick or a villain. It can be any genre of story. Make it short and concise, no more than 600 characters long. Don't put any images and don't show any html formatting";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
-  let character = document.querySelector("#character");
-  character.innerHTML = "Generating a character for you...";
+  let characterDescription = document.querySelector("#character-description");
+  characterDescription.innerHTML = `<div class = "generating">Generating a new character based on the keyword(s) "${keywords.value}"...</div>`;
   axios.get(apiUrl).then(createCharacter);
+
+  let characterBox = document.querySelector("#character-box");
+  characterBox.classList.remove("hidden");
 }
 
 let characterGenerator = document.querySelector("#character-generator");
